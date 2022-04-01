b7dc522dad75cdfee2de092d9ac0768a4fac100b
chukwuyem11 <chukwuyem99@gmail.com>
2022-04-01 16:53:04 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import '../styles/globals.css'
 import {QueryClient, QueryClientProvider} from "react-query"
-import { Provider } from 'next-auth/client';
+// import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }) {
   const queryClient = new QueryClient()
 
   return (
-    <Provider session={pageProps.session}>
     <QueryClientProvider client={queryClient}>
-  <Component {...pageProps} /></QueryClientProvider></Provider>)
+  <Component {...pageProps} /></QueryClientProvider>)
 }
 
 export default MyApp
